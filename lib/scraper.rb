7fa00765525a5eb67d42b0f1210ccb6b5f278b84
Drew Thompson <drewallynthompson@gmail.com>
2020-09-11 03:00:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,28 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = doc.css(".post").first.css("h2").text
+      course.schedule = doc.css(".post").first.css(".date").text
+      course.description = doc.css(".post").first.css("p").text
+    end
+    binding.pry
+  end
+  
+  
+  def get_courses
+  end 
+  
+  def make_courses
+  end 
   
   def print_courses
     self.make_courses
@@ -17,6 +36,8 @@ class Scraper
   end
   
 end
+
+Scraper.new.get_page
 
 
 
